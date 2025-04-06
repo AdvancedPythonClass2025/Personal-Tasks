8f9f17175b6b008155ac32e5af860438d96629be
Mat-84 <matinnor084@gmail.com>
2025-04-06 18:35:09 +0330

    sakht def namyesh va nemomneh az class

--- Matin Nouranian/Session-2/task2.py
+++ Matin Nouranian/Session-2/task2.py
@@ -3,4 +3,9 @@ class Car :
         self.model = model
         self.rang = rang
         self.salsakht = salsakht
-        
+    def namayesh_eteleat(self):
+        print("""model mashine : {}
+               rang : {}
+              salsakht : {}""".format(self.model,self.rang,self.salsakht))
+car = Car("pride","sefid","1380")
+car.namayesh_eteleat()
