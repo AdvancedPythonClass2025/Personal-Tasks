ae945d294322c9354c7c732a1458ddd749df0a03
Mat-84 <matinnor084@gmail.com>
2025-04-06 20:21:26 +0330

    use os and write def to clear terminal

--- Matin Nouranian/Session-2/task2.py
+++ Matin Nouranian/Session-2/task2.py
@@ -1,3 +1,6 @@
+import os
+def Clear():
+    os.system("cls" if os.name == "nt" else "clear")
 class Car :
     def __init__(self,model,rang,salsakht):
         self.model = model
@@ -5,5 +8,6 @@ class Car :
         self.salsakht = salsakht
     def namayesh_eteleat(self):
         print("model mashine : {} \nrang : {} \nsalsakht : {}".format(self.model,self.rang,self.salsakht))
+Clear()
 car = Car("pride","sefid","1380")
 car.namayesh_eteleat()
