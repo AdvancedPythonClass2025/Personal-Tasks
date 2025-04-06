ef99aef92c7e24b0701e7aa14af98be1e9efe86b
Mat-84 <matinnor084@gmail.com>
2025-04-06 21:55:08 +0330

    last update of task

--- Matin Nouranian/Session-2/task2.py
+++ Matin Nouranian/Session-2/task2.py
@@ -7,14 +7,19 @@ class Car :
         self.rang = rang
         self.salsakht = salsakht
     def namayesh_eteleat(self):
-        print("model mashine : {} \nrang : {} \nsalsakht : {}".format(self.model,self.rang,self.salsakht))
+        print("model mashine : {} \n\nrang : {} \n\nsalsakht : {}\n".format(self.model,self.rang,self.salsakht))
     def taghir_rang(self,rang_jadid):
         self.rang = rang_jadid
-        print('rang mashine be ---{}--- taghire kard'.format(rang_jadid))
+        print('rang {} be ---{}--- taghire kard\n----------------------------'.format(self.model,rang_jadid))
 
 car = Car("pride","sefid","1380")
 car2 = Car("lamborgini","meshki","2023")
 car3 = Car('partol',"gray","2000")
 Clear()
+
 car.namayesh_eteleat()
-car.taghir_rang('meshki')
+car.taghir_rang('ghermez')
+car2.namayesh_eteleat()
+car2.taghir_rang('abi')
+car3.namayesh_eteleat()
+car3.taghir_rang('meshki')
