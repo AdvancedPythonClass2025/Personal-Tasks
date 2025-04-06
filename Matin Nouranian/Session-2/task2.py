c7d3f4883a87117e4f8835b6e44c002a434179ea
Mat-84 <matinnor084@gmail.com>
2025-04-06 18:10:45 +0330

    meghdardehi avalie

--- Matin Nouranian/Session-2/task2.py
+++ Matin Nouranian/Session-2/task2.py
@@ -1,3 +1,6 @@
 class Car :
-    def __init__(self):
-        pass
+    def __init__(self,model,rang,salsakht):
+        self.model = model
+        self.rang = rang
+        self.salsakht = salsakht
+        
